1636df831b8f168091021a3ddaf3551e36f226be
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-01-08 07:07:48 +0530

    .

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,41 @@
-print("Hello World");
+print("Hello World")
 
 a = int(input("Enter first number: "))
 b = int(input("Enter second  number: "))
 
 print(a+b)
+
+print(type(a))
+
+# b=None
+"""
+print(b)
+
+print(type(b)) """
+
+h=98
+
+print(h)
+
+
+m=10
+n=2
+
+n **= n
+print(n)
+
+print(m**n)
+
+
+#Logical operators
+print(not True)
+
+print(not (m>n))
+
+val1=True
+val2=False
+print("and operator: ",val1 and val2)
+
+print("or operator: ",val1 or val2)
+
+print("And operator: ",(m==n) and (m>n))
---
+++ satis.py
@@ -0,0 +1,29 @@
+#Type Conversion
+a=8
+b=9.8
+
+sum = a+b
+
+print(sum)
+
+#Type Casting
+
+p=29
+q="9"
+c=int(q)
+su=p+c
+print(su)
+
+
+
+# Taking input od two numbers and printing their multiplication
+
+var1 = int(input("Enter num 1: "))
+var2 = int(input("Enter num 2: "))
+
+mul = var1*var2
+
+print("Multiplication of two numbers is : ", mul)
+
+
+print(var1>=var2)
