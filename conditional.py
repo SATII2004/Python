84320f50625e970e36dcd7ab0f80663036ecc58b
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-02-13 20:08:46 +0530

    .

---
+++ conditional.py
@@ -0,0 +1,8 @@
+light="green"
+
+if(light=="red"):
+     print("stop")
+elif(light=="yellow"):
+     print("ready")
+else:
+     print("go")
--- strings.py
+++ strings.py
@@ -1,3 +1,16 @@
 str = "apna college"
 
 print(str[0:3])
+
+print(str[-3:-1])  #negative index
+
+
+sem= " I am a studying python."
+
+print(sem.endswith("er"))
+print(sem.capitalize())
+
+print(sem.replace("y","z"))
+
+print(sem.find("am"))
+print(sem.count("a"))
