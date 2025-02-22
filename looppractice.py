1e45d272fec836da2fb31bb5f467cd6c0ded972c
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-02-22 19:26:37 +0530

    .

---
+++ looppractice.py
@@ -0,0 +1,8 @@
+
+#printing even numbers
+
+for i in range(2,100,2):
+    print(i)
+
+
+set
---
+++ whileloop.py
@@ -0,0 +1,27 @@
+age = int(input("Enter your age: "))
+
+while age < 0:
+    print("Age can't be negative")
+    age = int(input("Enter your age: "))
+
+print(f"You are {age} years old.")
+
+
+food= input("Enter a food you like (q to quit): ")
+
+while not food == "q":
+    print(f"You like {food}")
+    food = input("Enter a food you like (q to quit): ")
+print("byee")
+
+
+num = int(input("Enter a number between 1 and 10: "))
+
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter a number between 1 and 10: "))
+print(f"Your number is {num}")
+
+
+
+
