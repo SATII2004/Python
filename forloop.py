277fab00846d8b8c529240bbc3e397dce52ae03e
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-02-22 19:05:10 +0530

    .

---
+++ forloop.py
@@ -0,0 +1,30 @@
+fru={"apple","banana","pineapple","guava"}
+
+print(len(fru))
+
+fru.add("grapes")
+fru.add("sfdfs")
+fru.add("eggres")
+fru.add("grapgrges")
+fru.add("grapesegeg")
+fru.add("grapeddbs")
+fru.add("grapesbr")
+fru.add("grapesscbn")
+fru.add("grapeeblss")
+fru.add("grapegef;ss")
+fru.add("grapefews")
+
+
+for i in fru:
+    print(i)
+
+# printing numbers from 100 to 1
+
+for i in range(100,0,-1):
+    print(i)
+
+for x in range(1,21):
+    if x == 13:
+        break
+    else:
+        print(x)
