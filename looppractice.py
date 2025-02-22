ea359bc5a1f20507ff76c539b3c2d1372334c2cf
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-02-22 22:06:45 +0530

    Update looppractice.py

--- looppractice.py
+++ looppractice.py
@@ -5,4 +5,27 @@ for i in range(2,100,2):
     print(i)
 
 
-set
+num=(29,41,29,90,77,8,91,29,25)
+
+for i in num:
+    if i==77:
+        print("Number found")
+    else:
+        print("Number not found")
+
+
+# pass statement
+
+for k in range(6):
+    pass
+print("Some useful work")
+
+
+
+# factorial of first n numbers
+n=5
+fact= 1
+for z in range(n,0,-1):
+    fact *= z
+
+print(fact)
