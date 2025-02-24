09da6d52e8f7dfb9c3ab2ea640e0ef4b4c0d3b2b
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-02-24 22:24:51 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,4 +1,4 @@
-
+from bisect import insort_right
 
 
 def mul(i,j):
@@ -19,3 +19,61 @@ def avg(a,b,c):
 output = avg(10,20,30)
 
 print(output)
+
+#print length of a list
+
+cities = ["delhi","gurgaon","noida","mumbai","chennai","bhopal","amritsar","pune","satara","angul"]
+
+def print_length(list):
+    print(len(list))
+
+print_length(cities)
+
+def print_ele(list):
+    print(list)
+
+print_ele(cities)
+
+print("###################")
+
+print(cities[3], end=" ")
+print(cities[7],end=" ")
+
+def print_list(list):
+    for item in list:
+        print(item,end=" ")
+
+print_list(cities)
+
+
+
+#factorial of n
+print( )
+
+def factorial(n):
+    fact=1
+    for i in range(n,0,-1):
+        fact *= i
+    return fact
+
+print(factorial(5))
+
+#usd to inr
+
+def curr_converter(usd_val):
+    inr = 83 * usd_val
+    return inr
+
+print(curr_converter(1))
+
+
+# check even or odd
+
+def even_odd():
+    n=int(input("Enter a number: "))
+    if n%2==0:
+        return "Even number"
+    else:
+        return "Odd number"
+
+print(even_odd())
