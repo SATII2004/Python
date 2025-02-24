a870503ed948520da3db89eba8272a5442a66ff5
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-02-24 20:52:19 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,21 @@
+
+
+
+def mul(i,j):
+    return i*j
+
+print(mul(23,34))
+
+def add(k,l):
+    print(f"Sum = {k+l}")
+
+add(30,10)  #function call
+
+
+def avg(a,b,c):
+    average=(a+b+c)/3
+    return average
+
+output = avg(10,20,30)
+
+print(output)
