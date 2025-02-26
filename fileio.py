ad1fd03bbfc9792e648a7472ea5d8e5cf9ba7f65
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-02-26 09:32:03 +0530

    Update fileio.py

--- fileio.py
+++ fileio.py
@@ -1,14 +1,21 @@
 f = open("demo.txt","r")
 
+
+line1 = f.readline()  # reads data line by line
+line2 = f.readline()
+
+
+print(line1)
+print("----------------------------------------------------------")
+
+print(line2)
+print("----------------------------------------------------------")
+
 data = f.read()
 daa = f.read(7)
-dat = f.readline()  # reads data line by line
-
-
-print(data)
-print(daa)
-print(dat)
-
 print(type(data))
+print(data)
+print("----------------------------------------------------------")
+print(daa)
 
 f.close()
